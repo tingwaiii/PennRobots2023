aae2adeff65d17f5d2c952f1f4d4d740772d7cdb
jsanderson1512 <47279179+jsanderson1512@users.noreply.github.com>
2023-03-14 00:17:26 -0400

    added yue physics and robot simulation

--- Penn Robots 2023/Assets/XR_Scripts/Robots_SendAListOfPoints.cs
+++ Penn Robots 2023/Assets/XR_Scripts/Robots_SendAListOfPoints.cs
@@ -8,15 +8,27 @@ public class Robots_SendAListOfPoints : MonoBehaviour
     public GameObject robotTarget_WorldSpace;
     public RobotController_Kinematics controllerRobot;
 
+    //simulation stuff
+    private int currentPoint;
+    private bool startSimulation;
+    private bool newPoint;
+    private float lerpPosStartTime;
+    public float timeToLerp = 0.5f;
+    private Vector3 lerpStartPosition;
+    private Quaternion lerpStartRotation;
+    private Vector3 lerpTargetPosition;
+    private Quaternion lerpTargetRotation;
+
+
     public void SendListOfPoints()
     {
-        for (int i = 0;i<listOfPointsParent.transform.childCount;i++)
+        for (int i = 0; i < listOfPointsParent.transform.childCount; i++)
         {
             robotTarget_WorldSpace.transform.position = listOfPointsParent.transform.GetChild(i).position;
             robotTarget_WorldSpace.transform.rotation = listOfPointsParent.transform.GetChild(i).rotation;
 
             controllerRobot.RobotTarget_RobotSpace.position = robotTarget_WorldSpace.transform.position;//set position in world to match the robot target
-                                                                                              //RobotTarget_RobotSpace.localPosition = RobotTarget_RobotSpace.localPosition + new Vector3(TCPDifferencePosition.x, TCPDifferencePosition.y, TCPDifferencePosition.z);
+                                                                                                        //RobotTarget_RobotSpace.localPosition = RobotTarget_RobotSpace.localPosition + new Vector3(TCPDifferencePosition.x, TCPDifferencePosition.y, TCPDifferencePosition.z);
 
             controllerRobot.RobotTarget_RobotSpace.rotation = robotTarget_WorldSpace.transform.rotation;//set position in world to match the robot target
 
@@ -25,5 +37,46 @@ public class Robots_SendAListOfPoints : MonoBehaviour
             GetComponent<MachinaActionController>().GoToRobotTarget();
         }
 
+    }
+    public void SimulateListOfPoints()
+    {
+        startSimulation = true;
+        newPoint = true;
+        currentPoint = 0;
+    }
+    private void Update()
+    {
+        if (startSimulation)
+        {
+            int childCount = listOfPointsParent.transform.childCount;
+
+            if (newPoint)
+            {
+                //set start conditions 
+                lerpStartPosition = robotTarget_WorldSpace.transform.position;
+                lerpTargetPosition = listOfPointsParent.transform.GetChild(currentPoint).position;
+                lerpStartRotation = robotTarget_WorldSpace.transform.rotation;
+                lerpTargetRotation = listOfPointsParent.transform.GetChild(currentPoint).rotation;
+                lerpPosStartTime = Time.time;
+                newPoint = false;
+            }
+
+            float percentageTimeElapsed = (Time.time - lerpPosStartTime) / timeToLerp;
+
+            // Set our position as a fraction of the distance between the markers.
+            robotTarget_WorldSpace.transform.position = Vector3.Lerp(lerpStartPosition, lerpTargetPosition, percentageTimeElapsed);
+            float posDiffNew = Vector3.Distance(lerpTargetPosition, robotTarget_WorldSpace.transform.position);
+            robotTarget_WorldSpace.transform.rotation = Quaternion.Lerp(lerpStartRotation, lerpTargetRotation, percentageTimeElapsed);
+
+            if (posDiffNew < 0.01f)
+            {
+                currentPoint++;
+                newPoint = true;
+                if (currentPoint > childCount-1)
+                {
+                    startSimulation = false;
+                }
+            }
+        }
     }
 }
